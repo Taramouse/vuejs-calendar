160f50db4e8b7c81a1961f897c210cea17c6a813
taramouse <nickykeyse@gmail.com>
2018-11-15 17:04:14 +0000

    Generating days of current month.

--- src/components/App.vue
+++ src/components/App.vue
@@ -1,17 +1,28 @@
 <template>
 <div>
-  {{ msg }}
+  <div v-for="day in days" :key="day">{{ day }}</div>
 </div>
 </template>
 
 <script>
 export default {
-data() {
-  return {
+  data() {
+    return {
       month: 11,
       year: 2018
     }
+  },
+  computed: {
+    days() {
+      // Generating all days in current month
+      let days = []
+      let currentDay = this.$moment(`${this.year}-${this.month}-1`, 'YYYY-M-D')
+      do {
+        days.push(currentDay)
+        currentDay = this.$moment(currentDay).add(1, 'days')
+      } while ((currentDay.month() + 1) === this.month)
+      return days
+    }
   }
 }
-
 </script>
