621f79bf4e26234ed72ea1c1d93a5a3e2209cb7f
taramouse <nickykeyse@gmail.com>
2018-11-16 11:31:14 +0000

    Added formatted date for month  and year.

--- src/components/CurrentMonth.vue
+++ src/components/CurrentMonth.vue
@@ -1,12 +1,22 @@
 <template>
   <div>
-    Current Month
+    {{formattedDate}}
   </div>
 </template>
 
 <script>
 export default {
-
+  computed: {
+    month() {
+      return this.$store.state.currentMonth
+    },
+    year() {
+      return this.$store.state.currentYear
+    },
+    formattedDate() {
+      return this.$moment(`${this.year}-${this.month}-1`, 'YYYY-M-D').format('MMMM YYYY')
+    }
+  }
 }
 </script>
 
