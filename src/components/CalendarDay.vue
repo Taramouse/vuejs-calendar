1dcf7f049066eb7a053cca160085b31978bb4db6
taramouse <nickykeyse@gmail.com>
2018-11-16 12:56:00 +0000

    Capture click events to reposition event form.

--- src/components/CalendarDay.vue
+++ src/components/CalendarDay.vue
@@ -1,9 +1,14 @@
 <template>
-<div :class="classObject">{{day.format('D')}}</div>
+<div :class="classObject" @click="captureClick">{{day.format('D')}}</div>
 </template>
 <script>
 export default {
   props: ['day'],
+  methods: {
+    captureClick(event) {
+      this.$store.commit('eventFormPos', { x: event.clientX, y: event.clientY })
+    }
+  },
   computed: {
     classObject() {
       let today = this.day.isSame(this.$moment(), 'day')
