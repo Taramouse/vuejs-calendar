d6ebdb7f864628701e7751b04b9eda6805ccb86a
taramouse <nickykeyse@gmail.com>
2018-11-16 11:39:13 +0000

    Added inc and dec buttons.

--- src/components/CurrentMonth.vue
+++ src/components/CurrentMonth.vue
@@ -1,11 +1,23 @@
 <template>
+<div>
   <div>
     {{formattedDate}}
   </div>
+  <button @click="dec">-</button>
+  <button @click="inc">+</button>
+</div>
 </template>
 
 <script>
 export default {
+  methods: {
+    dec() {
+      console.log('dec')
+    },
+    inc() {
+      console.log('inc')
+    }
+  },
   computed: {
     month() {
       return this.$store.state.currentMonth
