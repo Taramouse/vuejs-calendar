d91a7bc362dcee376e5e535d8089aae43f73fc6a
taramouse <nickykeyse@gmail.com>
2018-11-17 15:36:44 +0000

    Refactored to common entry file for server and client (web).

--- src/web.entry.js
+++ src/web.entry.js
@@ -1,17 +1,7 @@
-import Vue from 'vue'
 import './style.scss'
-
-import store from './store'
 
 import moment from 'moment-timezone'
 moment.tz.setDefault('UTC')
-Object.defineProperty(Vue.prototype, '$moment', {
-  get() {
-    return this.$root.moment
-  }
-})
-
-import App from './components/App.vue'
 
 // Get events which were injected into html by server
 let events = window.__INITIAL_STATE__.map(event => {
@@ -21,25 +11,5 @@ let events = window.__INITIAL_STATE__.map(event => {
   }
 })
 
-let initialState = Object.assign({}, store.state, { events })
-store.replaceState(initialState)
-
-new Vue({
-  el: '#app',
-  data: {
-    moment
-  },
-  components: {
-    App
-  },
-  store,
-  render(createElement) {
-    return createElement(
-      'div',
-      { attrs: { id: 'app' } },
-      [
-        createElement('app')
-      ]
-    )
-  }
-})
+import VueCalendar from './entry'
+VueCalendar(events).$mount('#app')
