9715568ed179f5f53a6ed68441f76dc9894ef99b
taramouse <nickykeyse@gmail.com>
2018-11-20 16:20:43 +0000

    Compiled for prodcution.

--- server.js
+++ server.js
@@ -20,6 +20,14 @@ let events = [
 
 let renderer;
 
+if(process.env.NODE_ENV === 'production') {
+  let bundle = fs.readFileSync('./dist/node.bundle.js', 'utf8');
+  renderer = require('vue-server-renderer').createBundleRenderer(bundle);
+  app.use('/dist', express.static(path.join(__dirname, 'dist')));
+}
+
+
+
 app.get('/', (req, res) => {
   let template = fs.readFileSync(path.resolve('./index.html'), 'utf-8');
   // Inject events into html
